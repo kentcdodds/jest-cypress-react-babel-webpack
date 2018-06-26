1a63e5f0e1d41813a04ddcc1751a69c577edc304
Kent C. Dodds <kent@doddsfamily.us>
2018-06-25 19:51:13 -0600

    fix babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,11 @@
 const isProd = String(process.env.NODE_ENV) === 'production'
+const isTest = String(process.env.NODE_ENV) === 'test'
 
 module.exports = {
-  presets: [['@babel/preset-env', {modules: false}], '@babel/preset-react'],
+  presets: [
+    ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
+    '@babel/preset-react',
+  ],
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-class-properties',
