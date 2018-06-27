1e3fb61af61f0a0a8e83ea42c4c43b5ed07e0f22
Kent C. Dodds <kent@doddsfamily.us>
2018-06-27 12:05:39 -0600

    improve eslint config

--- other/jest.lint.js
+++ other/jest.lint.js
@@ -5,4 +5,5 @@ module.exports = {
   displayName: 'lint',
   runner: 'jest-runner-eslint',
   testMatch: ['<rootDir>/**/*.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/coverage/', '/dist/'],
 }
