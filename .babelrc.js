461b7fc0baf07c49a0c89c7559360b8d4301fb56
Kent C. Dodds <kent@doddsfamily.us>
2018-06-30 15:41:51 -0600

    fix plugin

--- .babelrc.js
+++ .babelrc.js
@@ -7,7 +7,7 @@ module.exports = {
     '@babel/preset-react',
   ],
   plugins: [
-    'babel-plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     [
