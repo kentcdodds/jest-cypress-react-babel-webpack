54ead321078c0c9245fdfe9d2ffe88ee0ef87a3b
Kent C. Dodds <kent@doddsfamily.us>
2018-06-30 19:55:56 -0600

    custom module resolution

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {render} from 'react-testing-library'
+import loadable from 'react-loadable'
 import Calculator from '../calculator'
 
-test('renders', () => {
+test('renders', async () => {
+  await loadable.preloadAll()
   render(<Calculator />)
 })
