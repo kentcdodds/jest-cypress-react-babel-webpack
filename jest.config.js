506e7dccafc47faef528f1c891aace3459642e28
Kent C. Dodds <kent@doddsfamily.us>
2018-06-30 20:53:10 -0600

    coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,18 @@ module.exports = {
   },
   setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 17,
+      branches: 4,
+      functions: 20,
+      lines: 17,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
