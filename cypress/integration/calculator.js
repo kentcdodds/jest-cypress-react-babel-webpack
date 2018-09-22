985b27958368041f418b2d1fed131901215db59f
Kent C. Dodds <kent@doddsfamily.us>
2018-09-22 15:47:46 -0600

    first test

---
+++ cypress/integration/calculator.js
@@ -0,0 +1,15 @@
+describe('anonymous calculator', () => {
+  it('can make calculations', () => {
+    cy.visit('http://localhost:8080')
+      .get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(3)')
+      .click()
+      .get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(4)')
+      .click()
+      .get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(4)')
+      .click()
+      .get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(5)')
+      .click()
+      .get('.mNQM6vIr72uG0YPP56ow5')
+      .should('have.text', '3')
+  })
+})
