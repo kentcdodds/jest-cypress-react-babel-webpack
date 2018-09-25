d12caee7760ff7a5172f0deee53ea12c2caf5af6
Kent C. Dodds <kent@doddsfamily.us>
2018-09-24 22:12:20 -0600

    Test user login with Cypress

---
+++ cypress/e2e/login.js
@@ -0,0 +1,35 @@
+import {buildUser} from '../support/generate'
+
+describe('login', () => {
+  it('should login an existing user', () => {
+    const user = buildUser()
+    cy.visit('/')
+      .findByText(/register/i)
+      .click()
+      .findByLabelText(/username/i)
+      .type(user.username)
+      .findByLabelText(/password/i)
+      .type(user.password)
+      .findByText(/submit/i)
+      .click()
+      .findByText(/logout/i)
+      .click()
+      // now our test can start...
+      .findByText(/login/i)
+      .click()
+      .findByLabelText(/username/i)
+      .type(user.username)
+      .findByLabelText(/password/i)
+      .type(user.password)
+      .findByText(/submit/i)
+      .click()
+      // now let's verify things are set after login.
+      .url()
+      .should('eq', `${Cypress.config().baseUrl}/`)
+      .window()
+      .its('localStorage.token')
+      .should('be.a', 'string')
+      .findByTestId('username-display')
+      .should('have.text', user.username)
+  })
+})
