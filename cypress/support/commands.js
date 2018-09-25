fb9b8d2150279ef65716dd23e38b78bb73ea5c3a
Kent C. Dodds <kent@doddsfamily.us>
2018-09-24 22:21:17 -0600

    Keeping tests isolated and focused with custom Cypress commands

---
+++ cypress/support/commands.js
@@ -0,0 +1,12 @@
+import {userBuilder} from './generate'
+
+Cypress.Commands.add('createUser', overrides => {
+  const user = userBuilder(overrides)
+  return cy
+    .request({
+      url: 'http://localhost:3000/register',
+      method: 'POST',
+      body: user,
+    })
+    .then(({body}) => body.user)
+})
