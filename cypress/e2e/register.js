a4bd2c0c90c332c862ad29c5cd84d6eee23eb05d
Kent C. Dodds <kent@doddsfamily.us>
2018-09-24 21:44:20 -0600

    add registration test and generate file

---
+++ cypress/e2e/register.js
@@ -0,0 +1,21 @@
+import {userBuilder} from '../support/generate'
+
+describe('registration', () => {
+  it('should register a new user', () => {
+    const user = userBuilder()
+    cy.visit('/')
+      .getByText(/register/i)
+      .click()
+      .getByLabelText(/username/i)
+      .type(user.username)
+      .getByLabelText(/password/i)
+      .type(user.password)
+      .getByText(/submit/i)
+      .click()
+      .url()
+      .should('eq', `${Cypress.config().baseUrl}/`)
+      .window()
+      .its('localStorage.token')
+      .should('be.a', 'string')
+  })
+})
