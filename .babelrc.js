9a1cbdab80fb28868ee10116468c32e1ed99e694
Kent C. Dodds <kent@doddsfamily.us>
2019-08-06 16:24:01 -0600

    fix babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,9 @@
 const isProd = String(process.env.NODE_ENV) === 'production'
+const isTest = String(process.env.NODE_ENV) === 'test'
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', {modules: false}],
+    ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
     '@babel/preset-react',
     [
       '@emotion/babel-preset-css-prop',
