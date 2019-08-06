a31af6f6be46ca9cacd4977cc6dc74acf315d187
Kent C. Dodds <kent@doddsfamily.us>
2019-08-06 16:29:30 -0600

    support style imports

---
+++ src/shared/__tests__/auto-scaling-text.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import AutoScalingText from '../auto-scaling-text'
+
+test('renders', () => {
+  render(<AutoScalingText />)
+})
