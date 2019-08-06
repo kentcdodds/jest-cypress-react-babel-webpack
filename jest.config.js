ff219da974191bd245585a2817228c64ef1d2e41
Kent C. Dodds <kent@doddsfamily.us>
2019-08-06 16:49:14 -0600

    css modules

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
 }
