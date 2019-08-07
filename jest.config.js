293c4acae33df59b059d0ec56fc1c927953c2c14
Kent C. Dodds <kent@doddsfamily.us>
2019-08-07 14:16:12 -0600

    coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -15,4 +15,18 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
   collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 15,
+      branches: 10,
+      functions: 15,
+      lines: 15,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      functions: 100,
+      lines: 100,
+    },
+  },
 }
