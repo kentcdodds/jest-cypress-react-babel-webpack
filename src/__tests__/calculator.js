6410f389adf60902397ae5255caff361fd0b2155
Kent C. Dodds <kent@doddsfamily.us>
2019-08-07 10:47:33 -0600

    add setup files

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import Calculator from '../calculator'
 
-test('renders', () => {
-  render(<Calculator />)
+test('the clear button switches from AC to C when there is an entry', () => {
+  const {getByText} = render(<Calculator />)
+  const clearButton = getByText('AC')
+
+  fireEvent.click(getByText(/3/))
+  expect(clearButton).toHaveTextContent('C')
+
+  fireEvent.click(clearButton)
+  expect(clearButton).toHaveTextContent('AC')
 })
