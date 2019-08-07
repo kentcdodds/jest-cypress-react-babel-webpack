1cd0842becfe8200173e4495dead25edfeceba8f
Kent C. Dodds <kent@doddsfamily.us>
2019-08-07 14:23:42 -0600

    simplify

--- test/jest.lint.js
+++ test/jest.lint.js
@@ -5,5 +5,4 @@ module.exports = {
   displayName: 'lint',
   runner: 'jest-runner-eslint',
   testMatch: ['<rootDir>/**/*.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/coverage/', '/dist/', '/other/'],
 }
