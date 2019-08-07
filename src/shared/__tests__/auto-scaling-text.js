1104af2ae445db2071158ebd9dcc1521da8ac9b5
Kent C. Dodds <kent@doddsfamily.us>
2019-08-07 13:34:45 -0600

    test utils

--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render} from 'calculator-test-utils'
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
---
+++ test/calculator-test-utils.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {render as rtlRender} from '@testing-library/react'
+import {ThemeProvider} from 'emotion-theming'
+import * as themes from '../src/themes'
+
+function render(ui, {theme = themes.dark, ...options} = {}) {
+  function Wrapper({children}) {
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  }
+  Wrapper.propTypes = {
+    children: PropTypes.node,
+  }
+
+  return rtlRender(ui, {wrapper: Wrapper, ...options})
+}
+
+export * from '@testing-library/react'
+// override the built-in render with our own
+export {render}
