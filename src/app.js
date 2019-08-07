778c7ca8c982f2c55194e3612dfa97cd6559b49f
Kent C. Dodds <kent@doddsfamily.us>
2019-08-07 13:29:43 -0600

    SETUP FOR NEXT LESSON

--- src/app.js
+++ src/app.js
@@ -1,18 +1,49 @@
 import React from 'react'
+import {ThemeProvider} from 'emotion-theming'
 import Calculator from './calculator'
+import * as themes from './themes'
 
 function App() {
+  const [theme, setTheme] = React.useState('dark')
+  const handleThemeChange = ({target: {value}}) => setTheme(value)
   return (
     <div>
-      <Calculator />
-      <div style={{marginTop: 30, textAlign: 'center'}}>
-        Calculator component{' '}
-        <a href="https://codepen.io/mjijackson/pen/xOzyGX">created</a>
-        {' by '}
-        <br />
-        <a href="https://twitter.com/mjackson">Michael Jackson</a> of{' '}
-        <a href="https://reacttraining.com/">React Training</a>
-      </div>
+      <ThemeProvider theme={themes[theme]}>
+        <Calculator />
+        <div style={{marginTop: 30}}>
+          <fieldset>
+            <legend>Theme</legend>
+            <label>
+              <input
+                onChange={handleThemeChange}
+                checked={theme === 'light'}
+                type="radio"
+                name="theme"
+                value="light"
+              />{' '}
+              light
+            </label>
+            <label>
+              <input
+                onChange={handleThemeChange}
+                checked={theme === 'dark'}
+                type="radio"
+                name="theme"
+                value="dark"
+              />{' '}
+              dark
+            </label>
+          </fieldset>
+        </div>
+        <div style={{marginTop: 30, textAlign: 'center'}}>
+          Calculator component{' '}
+          <a href="https://codepen.io/mjijackson/pen/xOzyGX">created</a>
+          {' by '}
+          <br />
+          <a href="https://twitter.com/mjackson">Michael Jackson</a> of{' '}
+          <a href="https://reacttraining.com/">React Training</a>
+        </div>
+      </ThemeProvider>
     </div>
   )
 }
