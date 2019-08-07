aa7f9a5f9bffa217e46be5372cf3369c81a3f4ae
Kent C. Dodds <kent@doddsfamily.us>
2019-08-07 09:46:25 -0600

    custom module resolution

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const path = require('path')
+
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
