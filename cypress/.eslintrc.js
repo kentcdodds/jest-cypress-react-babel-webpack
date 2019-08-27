8100d81efb05491ca22bb4bb3e3f99da0e8bd928
Kent C. Dodds <kent@doddsfamily.us>
2019-08-27 14:27:46 -0600

    use recommended

--- cypress/.eslintrc.js
+++ cypress/.eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   root: true,
   plugins: ['eslint-plugin-cypress'],
+  extends: ['kentcdodds', 'kentcdodds/import', 'plugin:cypress/recommended'],
   env: {'cypress/globals': true},
-  rules: {
-    'cypress/no-assigning-return-values': 'error',
-    'cypress/no-unnecessary-waiting': 'error',
-    'cypress/assertion-before-screenshot': 'warn',
-  },
 }
