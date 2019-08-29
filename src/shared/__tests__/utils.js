4fcfaa26582657877e27e396e054e9543263e939
Kent C. Dodds <kent@doddsfamily.us>
2019-08-28 22:20:45 -0600

    add coverage

--- src/shared/__tests__/utils.js
+++ src/shared/__tests__/utils.js
@@ -2,4 +2,5 @@ import {getFormattedValue} from '../utils'
 
 test('formats the value', () => {
   expect(getFormattedValue('1234.0')).toBe('1,234.0')
+  expect(getFormattedValue('.10')).toBe('0.10')
 })
